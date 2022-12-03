a19b8e5f0c6cc213fa34db3dd6d2686a216a44b1
Rashed Ahmed <rashed.ahmed@email.com>
2022-12-03 14:17:41 +0600

    header > search results

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GoSearch } from 'react-icons/go'
+import { MdBolt } from 'react-icons/md'
 
 const Header = () => {
 	return (
@@ -8,7 +9,21 @@ const Header = () => {
 			<p>Daily Laughs for you and yours</p>
 			<div className='input-box'>
 				<input type='text' placeholder='How can we make you laugh today?' />
-				<GoSearch />
+				<GoSearch className='float' />
+				<div className='search-results'>
+					<div>
+						<MdBolt className='popular' />
+						<span>Social Jokes: The Crazy Teacher</span>
+					</div>
+					<div>
+						<MdBolt className='trending' />
+						<span>Social Jokes: The Crazy Teacher</span>
+					</div>
+					<div>
+						<MdBolt className='epic' />
+						<span>Social Jokes: The Crazy Teacher</span>
+					</div>
+				</div>
 			</div>
 		</header>
 	)

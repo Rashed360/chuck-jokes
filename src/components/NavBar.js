97f4f0beabb32540277acf062ce1fc7ee0d302bd
Rashed Ahmed <rashed.ahmed@email.com>
2022-12-03 14:53:38 +0600

    navbar & header floating options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
 	return (
 		<nav>
 			<div className='container'>
-				<ul>
+				<ul id='navbar'>
 					<li>
 						<a href='#'>So Funktioniert's</a>
 					</li>
@@ -16,6 +16,12 @@ const NavBar = () => {
 						<FaUser />
 						<span>Mein Berrich</span>
 						<FaCaretDown />
+						<ul id='options'>
+							<li>My published jokes</li>
+							<li>My saved jokes</li>
+							<li>Account Information</li>
+							<li>Publish new joke</li>
+						</ul>
 					</li>
 				</ul>
 			</div>
